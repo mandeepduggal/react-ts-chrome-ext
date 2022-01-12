2e0c3dde14ced98e78c1deeeee69ac03299e8542
mandeepduggal <mandeepduggal.63@gmail.com>
2022-01-12 14:02:38 +0530

    Added hot module reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './styles.css'
+import { ClickCount } from './ClickCounter'
 import IMAGE from './react.png'
 import LOGO from './logo.svg'
+// Link for react refresh plugin for hot module reload - https://github.com/pmmmwh/react-refresh-webpack-plugin
 
 export const App = () => {
     return <>
-        <h1>Hello - {process.env.NODE_ENV} {process.env.name}</h1>
+        
+        <h1>Hello121 - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Img" width="300" height="200" />
-        <img src={LOGO} alt="React Logo" width="300"/>
+        <img src={LOGO} alt="React Logo" width="300" />
+        <ClickCount />
     </>
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,17 @@
 const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    devServer: {
+        hot:true, // for hot reload
+        open:true // replacement of --open in command in package.json
+    },
     devtool: 'cheap-module-source-map',
     plugins: [
         new webpack.DefinePlugin({
             'process.env.name':JSON.stringify('Development')
-        })
+        }),
+        new ReactRefreshWebpackPlugin()
     ]
 }

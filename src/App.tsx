a7e557d2b24de307b888ee14eed930efca1038fb
mandeepduggal <mandeepduggal.63@gmail.com>
2022-01-12 11:52:27 +0530

    addes styling, imsges, svg in webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export const App = () =>{
-    return <h1>Hello</h1>
+import './styles.css'
+import IMAGE from './react.png'
+import LOGO from './logo.svg'
+
+export const App = () => {
+    return <>
+        <h1>Hello</h1>
+        <img src={IMAGE} alt="React Img" width="300" height="200" />
+        <img src={LOGO} alt="React Logo" width="300"/>
+    </>
 }

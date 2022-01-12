e22f4f5e34209daacf7e2b759f46522878dccec3
mandeepduggal <mandeepduggal.63@gmail.com>
2022-01-12 12:48:25 +0530

    setup seprate dev/prod webpack

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import LOGO from './logo.svg'
 
 export const App = () => {
     return <>
-        <h1>Hello</h1>
+        <h1>Hello - {process.env.NODE_ENV} {process.env.name}</h1>
         <img src={IMAGE} alt="React Img" width="300" height="200" />
         <img src={LOGO} alt="React Logo" width="300"/>
     </>

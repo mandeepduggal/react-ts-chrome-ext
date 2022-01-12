cfd2c3501c3a4c6c863017dc66180183d6507012
mandeepduggal <mandeepduggal.63@gmail.com>
2022-01-12 19:14:13 +0530

    Added Preetier

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import LOGO from './logo.svg'
 // Link for react refresh plugin for hot module reload - https://github.com/pmmmwh/react-refresh-webpack-plugin
 
 export const App = () => {
-    return <>
-        <h1>Hello121 - {process.env.NODE_ENV} {process.env.name}</h1>
-        <img src={IMAGE} alt="React Img" width="300" height="200" />
-        <img src={LOGO} alt="React Logo" width="300" />
-        <ClickCount />
+  return (
+    <>
+      <h1>
+        Hello121 - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+      <img src={IMAGE} alt="React Img" width="300" height="200" />
+      <img src={LOGO} alt="React Logo" width="300" />
+      <ClickCount />
     </>
+  )
 }
